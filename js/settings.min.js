0b5942e532bae28fb6dd552e6fde7856e1d1d22d
layotte <layotte@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 03:23:47 +0000

    Tagging 1.0.11
    git-svn-id: https://plugins.svn.wordpress.org/pigeon-pack/tags/1.0.11@1066607 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/settings.min.js
@@ -0,0 +1,1 @@
+var $pigeonpack_settings=jQuery.noConflict();$pigeonpack_settings(document).ready(function($){$('input[name=smtp_enable]').live('change',function(){if('mail'===$(this).val())$('tr.smtp_options').hide();else $('tr.smtp_options').show()})});
